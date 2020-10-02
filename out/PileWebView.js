84940ac341059f01907e2c318f1370e9214226c6
simonJOURDAN <simon.jourdan@gmail.com>
2020-10-02 10:25:30 +0200

    doc + debut pilewebview

---
+++ out/PileWebView.js
@@ -0,0 +1,9 @@
+"use strict";
+//================================ Class Executor ================================//
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.PileWebView = void 0;
+//--------------------------------------------------------------------------------//
+class PileWebView {
+}
+exports.PileWebView = PileWebView;
+//# sourceMappingURL=PileWebView.js.map
