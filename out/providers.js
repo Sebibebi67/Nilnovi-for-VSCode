6de21dd597c8769a1fd5f3a66495a98f0ea2eda2
Darius1325 <adamriviere@gmail.com>
2020-10-23 11:54:18 +0200

    adding doc

--- out/providers.js
+++ out/providers.js
@@ -1,11 +1,33 @@
 "use strict";
+//================================= providers.ts =================================//
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.hovers = exports.autoCompletion = void 0;
+//--------------------------------- Description ----------------------------------//
+//
+// This file regroups the methods used to provide some user-friendly features
+//
+//--------------------------------------------------------------------------------//
+//----------------------------------- Authors ------------------------------------//
+//
+// Adam RIVIERE
+//
+//--------------------------------------------------------------------------------//
+//----------------------------------- Imports ------------------------------------//
 const vscode = require("vscode");
+//--------------------------------------------------------------------------------//
+//------------------------------------ Methods -----------------------------------//
+/**
+   * Description : Provides auto-compltion for the Nilnovi language
+   *
+   * @returns The provider to push
+   *
+   * @author Adam RIVIERE
+*/
 function autoCompletion() {
     return vscode.languages.registerCompletionItemProvider('nilnovi', {
         provideCompletionItems(document, position, token, context) {
             const completions = [];
+            // Here we define every word we want to be auto-completed and we give it a category
             completions.push(new vscode.CompletionItem('begin', vscode.CompletionItemKind.Keyword));
             completions.push(new vscode.CompletionItem('end', vscode.CompletionItemKind.Keyword));
             completions.push(new vscode.CompletionItem('return', vscode.CompletionItemKind.Keyword));
@@ -28,18 +50,27 @@ function autoCompletion() {
             completions.push(new vscode.CompletionItem('boolean', vscode.CompletionItemKind.TypeParameter));
             completions.push(new vscode.CompletionItem('true', vscode.CompletionItemKind.Constant));
             completions.push(new vscode.CompletionItem('false', vscode.CompletionItemKind.Constant));
-            completions.push(new vscode.CompletionItem('get(n);', vscode.CompletionItemKind.Function));
-            completions.push(new vscode.CompletionItem('put(n);', vscode.CompletionItemKind.Function));
+            completions.push(new vscode.CompletionItem('get', vscode.CompletionItemKind.Function));
+            completions.push(new vscode.CompletionItem('put', vscode.CompletionItemKind.Function));
             return completions;
         }
     });
 }
 exports.autoCompletion = autoCompletion;
+/**
+   * Description : Provides hovers for the Nilnovi functions
+   *
+   * @returns The provider to push
+   *
+   * @author Adam RIVIERE
+*/
 function hovers() {
     return vscode.languages.registerHoverProvider('nilnovi', {
         provideHover(document, position, token) {
+            // First we get the position of the user's cursor to display the hover
             const range = document.getWordRangeAtPosition(position);
             const word = document.getText(range);
+            // Then we define the hover to display (only for the two existing Nilnovi functions)
             switch (word) {
                 case "put":
                     return new vscode.Hover({
@@ -58,4 +89,6 @@ function hovers() {
     });
 }
 exports.hovers = hovers;
+//--------------------------------------------------------------------------------//
+//================================================================================//
 //# sourceMappingURL=providers.js.map
