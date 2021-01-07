95c8435d9b074c71f15938edc1d021e24ec2cb00
Sebibebi67 <sebibebi67@gmail.com>
2021-01-07 14:54:02 +0100

    external html

--- out/PileWebViewPanel.js
+++ out/PileWebViewPanel.js
@@ -15,13 +15,14 @@ exports.PileWebViewPanel = void 0;
 //----------------------------------- Imports ------------------------------------//
 const vscode = require("vscode");
 const path = require("path");
+const fs_1 = require("fs");
 //--------------------------------------------------------------------------------//
 class PileWebViewPanel {
     constructor(context) {
         this.panel = vscode.window.createWebviewPanel("pile", "Pile éxecution", { viewColumn: vscode.ViewColumn.Beside, preserveFocus: true }, {
             enableScripts: true,
             retainContextWhenHidden: true,
-            localResourceRoots: [vscode.Uri.file(path.join(context.extensionPath, '/src/Webview'))]
+            localResourceRoots: [vscode.Uri.file(path.join(context.extensionPath, './Webview'))]
         });
         this.panel.webview.html = this.getWebviewContent(this.panel.webview, context);
         this.panel.onDidDispose(() => {
@@ -38,125 +39,9 @@ class PileWebViewPanel {
         this.instance = undefined;
     }
     getWebviewContent(webview, context) {
-        return `
-        <!DOCTYPE html>
-            <html lang="fr">
-
-            <head>
-                <meta charset="UTF-8">
-                <meta name="viewport" content="width=device-width, initial-scale=1.0">
-                <link rel="stylesheet" type="text/css" href="${webview.asWebviewUri(vscode.Uri.file(path.join(context.extensionPath, 'src/Webview', 'style.css')))}">
-                <title>Cat Coding</title>
-            </head>
-
-            <body>
-                <div class='div'>
-                    <div>
-                        <h1>Code Machine</h1>
-                        <p id='instruction'></p>
-                        <table id='instructionList'>
-                            <tbody id = 'instructionListBody'></tbody>
-                        </table>
-                    </div>
-                    <div>
-                        <h1>Pile d'exécution</h1>
-                        <table id='pileExecution'>
-                            <thead><tr><td>Indice pile</td><td>Pile d'exécution</td></tr></thead>
-                            <tbody  id='pileBody'></tbody>
-                        </table>
-                        <table id='legende'><tbody>
-                            <tr>
-                                <td>Couleurs:</td>
-                                <td class='integer'>Entier</td>
-                                <td class='boolean'>Booléen</td>
-                                <td class='address'>Adresse</td>
-                                <td class='block'>Bloc de liaison</td>
-                            </tr>
-                        </tbody></table>
-                    </div> 
-                </div>
-
-                <script>
-
-                    const vscode = acquireVsCodeApi();
-                    // Handle the message inside the webview
-
-                    window.addEventListener('message', event => {
-
-                        const message = event.data; // The JSON data our extension sent
-
-                        switch (message.command) {
-
-                            case 'showPile':
-                                table = document.getElementById('pileExecution');
-                                tableBody = document.getElementById('pileBody');
-                                tableBody.innerHTML = "" //Reset Pile
-                                
-                                message.pile.forEach(element => {
-                                    let tr = document.createElement('tr');
-                                    let num = document.createElement('td');
-                                    num.innerHTML = table.rows.length - 1;
-                                    if(message.pointer == num.innerHTML){
-                                        num.innerHTML = 'ip -> '+num.innerHTML;
-                                    }
-                                    tr.appendChild(num);
-                                    let content = document.createElement('td');
-                                    if(element.type != 'link'){
-                                        content.innerHTML = "<span class=" + element.type + ">" + element.value + "</span>";
-                                    }else{
-                                        content.innerHTML = 
-                                            "<span class=" + element.type + ">" + element.value + "</span>" + 
-                                            " -> (" +
-                                            "<span class=" + message.pile[element.value]['type'] +">" + message.pile[element.value]['value'] + "</span>)";
-                                    }
-                                    tr.appendChild(content);
-                                    tableBody.insertBefore(tr, tableBody.firstChild);
-                                })
-                                
-                                let highlighted = document.getElementById('highlighted');
-                                highlighted.classList.remove('highlighted');
-                                highlighted.removeAttribute("id");
-                                document.getElementById('instructionListBody').children[message.instructionLine-1].classList.add('highlighted');
-                                document.getElementById('instructionListBody').children[message.instructionLine-1].id = "highlighted";
-                                break;
-
-                            case 'showInstructionList':
-
-                            
-                                let instructionListBody = document.getElementById('instructionListBody');
-                                let debug = document.getElementById('debug');
-
-
-                                instructionListBody.innerHTML = "";
-
-                                for (let i = 0; i< message.list.length; i++){
-
-                                    let instruction = message.list[i];    
-                                                                 
-                                    let num = document.createElement('td');
-                                    num.classList.add("num");
-                                    
-                                    num.innerHTML = (i+1)+" - ";
-
-                                    let tr = document.createElement('tr');
-                                    let line = document.createElement('td');
-
-                                    line.innerHTML = instruction.machineCode;
-              
-                                    tr.appendChild(num);
-                                    tr.appendChild(line);
-                                    instructionListBody.appendChild(tr);
-                                    
-                                }
-                                break;
-                        }
-
-                    });
-                </script>
-
-            </body>
-
-        </html>`;
+        let htmlFile = fs_1.readFileSync("./src/Webview/pileDisplay.html", "utf-8");
+        console.log(webview.asWebviewUri(vscode.Uri.file(path.join(context.extensionPath, 'src/Webview', 'style.css'))));
+        return htmlFile;
     }
 }
 exports.PileWebViewPanel = PileWebViewPanel;
--- src/PileWebViewPanel.ts
+++ src/PileWebViewPanel.ts
@@ -35,7 +35,7 @@ export class PileWebViewPanel {
             {
                 enableScripts: true,
                 retainContextWhenHidden: true,
-                localResourceRoots: [vscode.Uri.file(path.join(context.extensionPath, '/src/Webview'))]
+                localResourceRoots: [vscode.Uri.file(path.join(context.extensionPath, './Webview'))]
             });
 
         this.panel.webview.html = this.getWebviewContent(this.panel.webview, context);
@@ -58,124 +58,8 @@ export class PileWebViewPanel {
     }
 
     private getWebviewContent(webview: vscode.Webview, context: Context) {
-        return `
-        <!DOCTYPE html>
-            <html lang="fr">
-
-            <head>
-                <meta charset="UTF-8">
-                <meta name="viewport" content="width=device-width, initial-scale=1.0">
-                <link rel="stylesheet" type="text/css" href="${webview.asWebviewUri(vscode.Uri.file(path.join(context.extensionPath, 'src/Webview', 'style.css')))}">
-                <title>Cat Coding</title>
-            </head>
-
-            <body>
-                <div class='div'>
-                    <div>
-                        <h1>Code Machine</h1>
-                        <p id='instruction'></p>
-                        <table id='instructionList'>
-                            <tbody id = 'instructionListBody'></tbody>
-                        </table>
-                    </div>
-                    <div>
-                        <h1>Pile d'exécution</h1>
-                        <table id='pileExecution'>
-                            <thead><tr><td>Indice pile</td><td>Pile d'exécution</td></tr></thead>
-                            <tbody  id='pileBody'></tbody>
-                        </table>
-                        <table id='legende'><tbody>
-                            <tr>
-                                <td>Couleurs:</td>
-                                <td class='integer'>Entier</td>
-                                <td class='boolean'>Booléen</td>
-                                <td class='address'>Adresse</td>
-                                <td class='block'>Bloc de liaison</td>
-                            </tr>
-                        </tbody></table>
-                    </div> 
-                </div>
-
-                <script>
-
-                    const vscode = acquireVsCodeApi();
-                    // Handle the message inside the webview
-
-                    window.addEventListener('message', event => {
-
-                        const message = event.data; // The JSON data our extension sent
-
-                        switch (message.command) {
-
-                            case 'showPile':
-                                table = document.getElementById('pileExecution');
-                                tableBody = document.getElementById('pileBody');
-                                tableBody.innerHTML = "" //Reset Pile
-                                
-                                message.pile.forEach(element => {
-                                    let tr = document.createElement('tr');
-                                    let num = document.createElement('td');
-                                    num.innerHTML = table.rows.length - 1;
-                                    if(message.pointer == num.innerHTML){
-                                        num.innerHTML = 'ip -> '+num.innerHTML;
-                                    }
-                                    tr.appendChild(num);
-                                    let content = document.createElement('td');
-                                    if(element.type != 'link'){
-                                        content.innerHTML = "<span class=" + element.type + ">" + element.value + "</span>";
-                                    }else{
-                                        content.innerHTML = 
-                                            "<span class=" + element.type + ">" + element.value + "</span>" + 
-                                            " -> (" +
-                                            "<span class=" + message.pile[element.value]['type'] +">" + message.pile[element.value]['value'] + "</span>)";
-                                    }
-                                    tr.appendChild(content);
-                                    tableBody.insertBefore(tr, tableBody.firstChild);
-                                })
-                                
-                                let highlighted = document.getElementById('highlighted');
-                                highlighted.classList.remove('highlighted');
-                                highlighted.removeAttribute("id");
-                                document.getElementById('instructionListBody').children[message.instructionLine-1].classList.add('highlighted');
-                                document.getElementById('instructionListBody').children[message.instructionLine-1].id = "highlighted";
-                                break;
-
-                            case 'showInstructionList':
-
-                            
-                                let instructionListBody = document.getElementById('instructionListBody');
-                                let debug = document.getElementById('debug');
-
-
-                                instructionListBody.innerHTML = "";
-
-                                for (let i = 0; i< message.list.length; i++){
-
-                                    let instruction = message.list[i];    
-                                                                 
-                                    let num = document.createElement('td');
-                                    num.classList.add("num");
-                                    
-                                    num.innerHTML = (i+1)+" - ";
-
-                                    let tr = document.createElement('tr');
-                                    let line = document.createElement('td');
-
-                                    line.innerHTML = instruction.machineCode;
-              
-                                    tr.appendChild(num);
-                                    tr.appendChild(line);
-                                    instructionListBody.appendChild(tr);
-                                    
-                                }
-                                break;
-                        }
-
-                    });
-                </script>
-
-            </body>
-
-        </html>`
+        let htmlFile = readFileSync("./src/Webview/pileDisplay.html", "utf-8");
+        console.log(webview.asWebviewUri(vscode.Uri.file(path.join(context.extensionPath, 'src/Webview', 'style.css'))));
+        return htmlFile;
     }
 }
